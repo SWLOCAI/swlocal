ddf6d7ef29695104ebd4ea6d0bc71f5f275f869b
leeeeunji <dldmswl6233@naver.com>
2022-09-12 01:36:16 +0900

    map modified

--- locationDB/models.py
+++ locationDB/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+
 class Post(models.Model):
-    
-    user_id = models.CharField(max_length = 10, null = False) # 사용자 이름
-    longitude = models.DecimalField(max_digits = 10, decimal_places = 7, null = False) # 경도값
-    latitude = models.DecimalField(max_digits = 10, decimal_places = 7, null = False) # 위도값
-    group_id = models.CharField(max_length = 20, null = True) # 그룹 코드
+
+    user_id = models.CharField(max_length=10, null=False)  # 사용자 이름
+    longitude = models.DecimalField(
+        max_digits=10, decimal_places=7, null=False)  # 경도값
+    latitude = models.DecimalField(
+        max_digits=10, decimal_places=7, null=False)  # 위도값
+    group_id = models.CharField(max_length=20, null=True)  # 그룹 코드
 
     def __str__(self):
         return f'[{self.pk}]{self.user_id}'
