c374324609f8ce4d685d3d50efe3cfe511753984
leeeeunji <dldmswl6233@naver.com>
2022-09-12 01:14:12 +0900

    map을 locationDB와 합침

--- map/urls.py
+++
@@ -1,6 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.map_main, name='map_main'),
-]
--- map/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def map_main(request):
-    return render(request, 'map_main.html')
