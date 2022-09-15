e022200429cc3148eb33f746694d86cff539542c
lhskh307218 <lhskh307218@gmail.com>
2022-09-15 23:31:10 +0900

    change urls.py

--- locationDB/urls.py
+++ locationDB/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view()),
-    path('map/', views.map_main, name='map_main'),
+    path('list/', views.PostList.as_view()),
+    path('', views.map_main, name='map_main'),
     path('map_user/', views.map_user, name='map_user'),
 ]
